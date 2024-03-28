5962f36e10f549c18da4cb4283156c700a3a78a4
raultromero <raultromero@gmail.com>
2024-03-28 22:17:23 +0100

    Add .env file and update the configuration file

--- server/config/index.js
+++ server/config/index.js
@@ -1,8 +1,20 @@
 // server/config/index.js
-
+/*
 const config = {
   server: {
     port: 3000,
+  },
+};
+*/
+
+// require('dotenv').config('');
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+  server: {
+    port: process.env.SERVER_PORT,
   },
 };
 
