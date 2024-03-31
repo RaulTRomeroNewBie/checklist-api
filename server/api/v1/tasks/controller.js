4daa77b41fded1cc534e002e1f58fe80a4064df5
raultromero <raultromero@gmail.com>
2024-03-31 12:14:25 +0200

    Standardized JSON responses

--- server/api/v1/tasks/controller.js
+++ server/api/v1/tasks/controller.js
@@ -28,7 +28,10 @@ export async function create(req, res, next) {
   try {
     const doc = await document.save();
     res.status(201);
-    res.json(doc);
+    res.json({
+        success: true,
+        data: doc,
+    });
   } catch (err) {
     next(new Error(err));
   }
@@ -38,7 +41,10 @@ export async function create(req, res, next) {
 export async function all(req, res, next) {
   try {
     const docs = await taskModel.find({}).exec();
-    res.json(docs)
+    res.json({
+      success: true, 
+      data: docs,
+    });
   } catch (err) {
     next(new Error(err));
   }
@@ -47,15 +53,26 @@ export async function all(req, res, next) {
 
 export async function read(req, res, next) {
   const { doc = {} } = req;
-  res.json(doc);
+  res.json({
+    success: true,
+    data: doc,
+  });
 };
 
 export async function update(req, res, next) {
   const { doc = {}, body = {} } = req;
+  // La función Object.assign mezcla el contenido del primer objeto doc con el
+  // contenido del segundo objeto body, solo a primer nivel, es decir, si tiene
+  // propiedades de mayor profundiad no lo realizará, por lo cual se recomienda
+  // para otros casos mas avanzados la función 'merge' de la librería 'lodash'.
+
   Object.assign(doc, body);
   try {
     const updated = await doc.save();
-    res.json(updated);
+    res.json({
+      success: true,
+      data: updated,
+    });
   } catch (err) {
     next(new Error(err));
   }
@@ -65,7 +82,10 @@ export async function erase(req, res, next) {
   const { doc = {} } = req;
   try {
     const removed = await doc.deleteOne();
-    res.json(removed);
+    res.json({
+      success: true,
+      data: removed,
+    });
   } catch (err) {
     next(new Error(err));
   }
